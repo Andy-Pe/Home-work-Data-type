2fb170d33d4176c547f0c043370de308ca032066
Andrey Petrov <Pandand1@yandex.ru>
2022-10-04 12:32:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long p = 2_500;
+        int m = 20;
+        System.out.println(p / m);
+        long x = p / m;
+    }
+}
