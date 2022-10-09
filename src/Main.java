9e8dfefbf51511a0caffe2719e1caefd8834ac13
Andrey Petrov <Pandand1@yandex.ru>
2022-10-09 14:37:10 +0300

    AutoTest

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        long price = 2_500;
-        int miles = 20;
-        System.out.println("Количество накопленных миль:");
-        System.out.println(price / miles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
