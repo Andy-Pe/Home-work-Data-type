01bd157a729d5807b18e2fae6732911ff6b9e24e
Andrey Petrov <Pandand1@yandex.ru>
2022-10-09 13:02:11 +0300

    making edits

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        long p = 2_500;
-        int m = 20;
-        System.out.println(p / m);
-        long x = p / m;
+        long price = 2_500;
+        int miles = 20;
+        System.out.println("Количество накопленных миль:");
+        System.out.println(price / miles);
     }
 }
